1f58f6fe9570e2612d3f6b7af0e8b0129edd3fcb
Irving Cabello <irvollo@gmail.com>
2019-03-29 17:00:09 -0600

    first commit

---
+++ src/components/LandingMessage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class LandingMessage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Bienvenida a Tecreo</h1>
+        <h2>No seas victima de nuevo.</h2>
+      </div>
+    )
+  }
+}
